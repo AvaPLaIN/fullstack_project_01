e6a2fede908b877f42ff22f239bb1decdf7356e8
Kevin Voß <kevin.voss@micromerce.com>
2025-05-12 19:13:24 +0200

    first commit

---
+++ backend/fullstack/src/test/kotlin/org/kevinvoss/fullstack/FullstackApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.kevinvoss.fullstack
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FullstackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "fullstack_project_01"
+include("backend")
